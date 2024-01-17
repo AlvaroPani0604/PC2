a53dbfdaa68290a769f30236753db015e8d04d51
AlvaroPani0604 <156676357+AlvaroPani0604@users.noreply.github.com>
2024-01-17 13:44:10 +0100

    Update docker.py

--- Mio pug/part2/docker.py
+++ Mio pug/part2/docker.py
@@ -20,18 +20,20 @@ def remp(fi, remp1, remp2):
 #Clonamos la carpeta practica_creativa2 del github
 subprocess.run(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2"])
 
-#Modificamos el fichero requirements.txt
+#Modificaciones del fichero requirements.txt con la funcion anterior
 remp('requirements.txt', 'urllib3==1.26.5', 'urllib3')
 remp('requirements.txt', 'chardet==3.0.4', 'chardet')
 remp('requirements.txt', 'gevent==1.4.0', 'gevent')
 remp('requirements.txt', 'greenlet==0.4.15', 'greenlet')
+
 #Instalamos pip sudo apt-get install python-pip
-subprocess.run(["apt-get", "install", "python3-pip"])
+subprocess.run(["sudo", "apt-get", "install", "python3-pip"])
+
 #Instalamos dependencias de requirements.txt con pip
 subprocess.run(["pip3", "install", '-r', './practica_creativa2/bookinfo/src/productpage/requirements.txt'])
 
-#Extraemos la variable de entorno (definida en el Dockerfile) y guardarla aqui
-title = os.environ.get('GROUPO_NUMERO')
+#Extraemos la variable de entorno y guardarla aqui
+title = os.environ.get('GRUPO_NUMERO')
 
 #Modificamos productpage.html para cambiar el titulo de la app por la variable de entorno
 remp('templates/productpage.html', 'Simple Bookstore App', title)
