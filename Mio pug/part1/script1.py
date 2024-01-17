be95eca1e64df9aa7d478b8cc05f2b8b8edc20e0
AlvaroPani0604 <156676357+AlvaroPani0604@users.noreply.github.com>
2024-01-17 13:41:58 +0100

    Update script1.py

--- Mio pug/part1/script1.py
+++ Mio pug/part1/script1.py
@@ -20,18 +20,22 @@ def remp(fi, remp1, remp2):
 #Clonamos la carpeta practica_creativa2 del github
 subprocess.run(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2"])
 
-#Modificamos el fichero requirements.txt
+#Modificaciones del fichero requirements.txt con la funcion anterior
 remp('requirements.txt', 'urllib3==1.26.5', 'urllib3')
 remp('requirements.txt', 'chardet==3.0.4', 'chardet')
 remp('requirements.txt', 'gevent==1.4.0', 'gevent')
 remp('requirements.txt', 'greenlet==0.4.15', 'greenlet')
+
 #Instalamos pip sudo apt-get install python-pip
 subprocess.run(["sudo", "apt-get", "install", "python3-pip"])
+
 #Instalamos dependencias de requirements.txt con pip
 subprocess.run(["pip3", "install", '-r', './practica_creativa2/bookinfo/src/productpage/requirements.txt'])
 
 #Creamos la variable de entorno (con nuestro num de grupo, 46)
+
 os.environ['GRUPO_NUMERO']= "46"
+
 #Extraemos la variable de entorno y guardarla aqui
 title = os.environ.get('GRUPO_NUMERO')
 
